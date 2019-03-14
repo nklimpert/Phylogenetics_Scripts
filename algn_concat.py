1678952b0afe5779f93d5fda2ab3025741072cd9
nklimpert <nathaniel.klimpert@botany.ubc.ca>
2019-03-13 17:17:06 -0700

    Update algn_concat.py to use argparse for improved clarity Also algn_concat.py can now produce partition files

--- algn_concat.py
+++ algn_concat.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
-'''USAGE: algn_concat.py <file1> <file2> <file3> ... > outFile'''
-
-#TODO : implement parseargs to deal with different formats
-#TODO : write a partition file based on the lengths of all the alignments
-
 import sys
 from Bio import AlignIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import generic_dna
 from Bio.Seq import Seq
+import collections
+import argparse
 
 
 def getTaxa(alignment):
@@ -22,38 +19,77 @@ def getTaxa(alignment):
 
 
 def main():
-    fileHandles = sys.argv[1:]
-    inFormat = 'fasta'
-    outFormat = 'fasta'
+    parser = argparse.ArgumentParser(description="Concatenate a set of multiple sequence alignments")
+    parser.add_argument("inFiles", nargs='+', metavar="<inFiles>",
+                        help="The alignments to be concatenated")
+    parser.add_argument("--infmt", choices=['fasta', 'nexus', 'phylip'],
+                        default='fasta', help="The file format of the input alignments -\
+                                 I can't handle multiple alignments with \
+                                 different formats, sorry!"
+                        )
+    parser.add_argument("--outfmt", choices=['fasta', 'nexus', 'phylip'],
+                        default='fasta', help="The file format of the output alignment -\
+                                 I can't handle multiple alignments with \
+                                 different formats, sorry!"
+                        )
+    parser.add_argument("-o", "--out", nargs='?', default=sys.stdout,
+                        help="Writes the concatenated matrix to the output file, if specified.\
+                              Otherwise, writes to stdout.")
+    parser.add_argument("-p", "--partitions",
+                        help="Use this option to save a partition file as partitions.txt")
+    args = parser.parse_args()
 
-    alignments = [AlignIO.read(file, inFormat) for file in fileHandles]
+    fileHandles = args.inFiles
+    inFormat = args.infmt
+    outFormat = args.outfmt
+    partitionFile = args.partitions
     
-    
+    # Storing all of the alignments in a dictionary as filehandle: alignment
+    alignments = collections.OrderedDict()
+    for file in fileHandles:
+        alignments[file] = AlignIO.read(file, inFormat)
+
     # This block should get all of the taxa across all taxa without any repeats
     matrixTaxa = set()
-    for alignment in alignments:
+    for alignment in alignments.values():
         # adds all the elements of the alignments set to the whole set
         matrixTaxa.update(getTaxa(alignment))
 
-    #next, need to add the missing taxa to each alignment
-    for alignment in alignments:
+    # next, need to add the missing taxa to each alignment
+    for alignment in alignments.values():
         missingTaxa = matrixTaxa - getTaxa(alignment)
         for taxon in missingTaxa:
-            #add a dummy alignment
+            # add a dummy alignment
             dummy = SeqRecord(Seq(alignment.get_alignment_length()*'-', generic_dna), id=taxon,
                               name=taxon)
             alignment.append(dummy)
     # need to sort all of the alignments after adding dummy sequences
-    map(lambda x: x.sort(), alignments)
-    combinedMatrix = alignments[0]
-    for alignment in alignments[1:]:
-        combinedMatrix = combinedMatrix + alignment
-    #TODO : find better way to silence the record descriptions
+    map(lambda x: x.sort(), alignments.values())
+    combinedMatrix = None
+    
+    # Writing all of the partition information to a dictionary
+    # Keeps track of the file handle, beginning, and end
+    partitions = collections.OrderedDict()
+
+    for filename, alignment in alignments.items():
+        if not combinedMatrix:
+            combinedMatrix = alignment
+            startPos = 1
+        else:
+            combinedMatrix = combinedMatrix + alignment
+        endPos = combinedMatrix.get_alignment_length()
+        partitions[filename] = (startPos, endPos)
+        startPos = endPos+1
+
+    # TODO : find better way to silence the record descriptions
     for record in combinedMatrix:
         record.description = ''
-    print(combinedMatrix.format(outFormat))
+    AlignIO.write(combinedMatrix, args.out, outFormat)
+    if(partitionFile):
+        with open(partitionFile, 'w') as outFile:
+            for filename, values in partitions.items():
+                outFile.write("{}: {}-{};\n".format(filename, values[0], values[1]))
 
 
 if __name__ == "__main__":
     main()
-    
